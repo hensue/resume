7d01f99a27ba1034e36decd3f62411ab2158521c
hensue <138180419+hensue@users.noreply.github.com>
2024-01-10 20:13:45 -0800

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,7 +14,7 @@ function Footer() {
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
-          <h3>Designed and Developed by Yassin Mohamed</h3>
+          <h3>Designed and Developed by Aleksa Vioglavin</h3>
         </Col>
         <Col md="4" className="footer-copywright">
           <h3>Copyright © {year} LN</h3>
